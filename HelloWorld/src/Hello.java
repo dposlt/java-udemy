be17f03bc1fe6565bc2ac0e69d3dbc3fe94e992a
Poslt, David (MONETA) <david.poslt@moneta.cz>
2020-07-30 12:28:31 +0200

    add challange - print name

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,23 @@
+
+import java.util.Scanner;
 public class Hello {
 
+    //chalange
+    public static String name() {
+        System.out.println("Enter yrou name: ");
+        Scanner sc = new Scanner(System.in);
+        String yourName = sc.nextLine();
+        return yourName;
+    }
+
+    public static void main(String args[]) {
+        String myName = name();
+        System.out.println("Hello " + myName);
+    }
+
+    /*
     public static void main(String args[]) {
         System.out.println("Udemy java");
-    }
+
+     */
 }
