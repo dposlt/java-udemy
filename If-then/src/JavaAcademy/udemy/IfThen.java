81c049d27550b20d0b1aa96f10fd47b585986c8f
dposlt <david.poslt@gmail.com>
2020-08-28 13:57:05 +0200

    if statemenst

---
+++ If-then/src/JavaAcademy/udemy/IfThen.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaAcademy.udemy;
+
+/**
+ *
+ * @author moula
+ */
+public class IfThen {
+    
+    public static void main(String args[]){
+        
+        // if statements
+          
+        boolean alien = false;
+        
+        if (alien == true)
+            System.out.println("It's not alien");
+            System.out.println("And I am scare aliens!"); //if is on the block, add only first row !!!
+        // result is row number 22
+    }
+    
+}
