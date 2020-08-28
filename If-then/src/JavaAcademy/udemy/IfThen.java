4e62888a7347ff8da36ccee11535d3943e17fa77
dposlt <david.poslt@gmail.com>
2020-08-28 14:22:15 +0200

    add block

--- If-then/src/JavaAcademy/udemy/IfThen.java
+++ If-then/src/JavaAcademy/udemy/IfThen.java
@@ -21,6 +21,11 @@ public class IfThen {
             System.out.println("It's not alien");
             System.out.println("And I am scare aliens!"); //if is on the block, add only first row !!!
         // result is row number 22
+
+        if (alien == false) {
+            System.out.println("It's not alien");
+            System.out.println("And I am scare aliens!"); //is add becouse is in the block !!!
+        }
     }
     
 }
