6ed7efa9676243761908d2726408212047bbdcf7
dposlt <david.poslt@gmail.com>
2020-09-07 10:53:21 +0200

    add paramaters

--- Methods/src/JavaAcademy/udemy/Main.java
+++ Methods/src/JavaAcademy/udemy/Main.java
@@ -3,17 +3,18 @@ package JavaAcademy.udemy;
 public class Main {
 
     public static void main(String[] args) {
-        calculateScore();
+        calculateScore(true,800, 5, 100);
     }
 
-    public static void calculateScore() {
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        /*
         boolean gameOver = true;
         int score = 800;
         int leveCompleted = 5;
         int bonus = 100;
-
+        */
         if (gameOver) {
-            int finalScore = score + (leveCompleted * bonus);
+            int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("Your finale score was " + finalScore);
         }
