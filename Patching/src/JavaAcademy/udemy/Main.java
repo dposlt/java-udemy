c68fabb11bf34a4b8ad4323616c38fdd62d3f810
Poslt, David (MONETA) <david.poslt@moneta.cz>
2020-09-16 14:07:31 +0200

    add grafics

--- Patching/src/JavaAcademy/udemy/Main.java
+++ Patching/src/JavaAcademy/udemy/Main.java
@@ -5,11 +5,14 @@ import java.io.FileNotFoundException;
 import java.io.OutputStream;
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        PrintOwnServers(getName());
+        //PrintOwnServers(getName());
+        Window();
 
     }
 
@@ -33,6 +36,20 @@ public class Main {
         String searchingString = searchString.nextLine().toLowerCase();
         searchingString = searchingString.substring(0,1).toUpperCase()+searchingString.substring(1);
         return searchingString;
+    }
+
+    public static void Window() {
+        JFrame window = new JFrame();
+        window.setSize(640,480);
+        window.setTitle("Search your servers");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //
+
+        JLabel label = new JLabel();
+        label.setText("Enter own name or server");
+        label.setVerticalTextPosition(20);
+        label.setHorizontalTextPosition(20);
+        window.setVisible(true);
     }
 
 }
