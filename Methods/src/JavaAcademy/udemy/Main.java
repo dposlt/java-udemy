54ac3c1c105566f6c0ff5b26623cb6189e3114d9
dposlt <david.poslt@gmail.com>
2020-09-24 10:56:11 +0200

    scorePositions challange

--- Methods/src/JavaAcademy/udemy/Main.java
+++ Methods/src/JavaAcademy/udemy/Main.java
@@ -6,8 +6,31 @@ public class Main {
         calculateScore(true,800, 5, 100);
 
         calculateScore(true,10000, 8, 200);
+
+        displayHighScorePosition("David", 1500);
+        displayHighScorePosition("Bob", 900);
+        displayHighScorePosition("Tim", 400);
+        displayHighScorePosition("Gilbert", 50);
+        displayHighScorePosition("Robert", 1000);
+
     }
 
+    public static void displayHighScorePosition(String name, int highScore){
+
+        System.out.println(name + " managed to get into position " + calculateHighScorePosition(highScore) + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500 && score < 1000) {
+            return 2;
+        } else if (score >= 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         /*
         boolean gameOver = true;
