41cb22adbd4e0d06be9c5b75ea97334aadfdfbb0
dposlt <david.poslt@gmail.com>
2020-09-29 19:30:55 +0200

    add arrays of servers

--- ServerStatus/src/JavaAcademy/udemy/Main.java
+++ ServerStatus/src/JavaAcademy/udemy/Main.java
@@ -6,12 +6,18 @@ import java.net.InetAddress;
 public class Main {
 
     public static void main(String[] args) {
+
+        String [] servers = {"server1","server2","server3","server4"};
+
         try {
-            if (ping("serverName")) {
-                System.out.println("Server is up");
-            } else {
-                System.out.println("Server is down");
+            for (byte i = 0; i < servers.length; i++){
+                if (ping(servers[i])) {
+                    System.out.println("Server is up");
+                } else {
+                    System.out.println("Server is down");
+                }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
