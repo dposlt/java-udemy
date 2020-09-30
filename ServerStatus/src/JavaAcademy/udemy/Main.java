f95ff6ac845b4bb8b95c86a2df78e8b5934f2915
Poslt, David (MONETA) <david.poslt@moneta.cz>
2020-09-30 08:57:09 +0200

    add sleep

--- ServerStatus/src/JavaAcademy/udemy/Main.java
+++ ServerStatus/src/JavaAcademy/udemy/Main.java
@@ -7,18 +7,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String [] servers = {"server1","server2","server3","server4"};
+        String [] servers = {"ufotomtst1x","ufotomtst2x","ufotomtst3x","ufotomtst4x","ufotomtst5x","ufotomtst6x","ufotomtst7x","ufotomtst8x"};
 
         try {
             for (byte i = 0; i < servers.length; i++){
                 if (ping(servers[i])) {
                     System.out.println("Server is up");
+                    cara("#");
+                    Thread.sleep(2000);
                 } else {
                     System.out.println("Server is down");
                 }
             }
 
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
@@ -26,7 +28,7 @@ public class Main {
     public static boolean ping(String serverName) throws IOException {
         try {
             InetAddress address = InetAddress.getByName(serverName);
-            System.out.println(address + ":");
+            System.out.println("\n" + address + ":");
             boolean reachable = address.isReachable(10000);
             if (reachable == true) return true;
         } catch (Exception e) {
@@ -34,5 +36,11 @@ public class Main {
         }
         return false;
 
+    }
+
+    public static void cara(String znak) {
+        for( byte i = 0; i < 30; i++) {
+            System.out.printf(znak);
+        }
     }
 }
