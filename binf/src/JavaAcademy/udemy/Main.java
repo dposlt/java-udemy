a8b420d4337eb38bdb3227cda6144a8115836cae
dposlt <david.poslt@gmail.com>
2020-10-25 07:27:18 +0100

    change menu

--- binf/src/JavaAcademy/udemy/Main.java
+++ binf/src/JavaAcademy/udemy/Main.java
@@ -12,10 +12,17 @@ public class Main {
     public static String showMenu() {
         String znak = "#";
 
+        /*
         int i;
         for(i=0; i<=20; i++) System.out.printf(znak);
         System.out.println("\n B I N F -- M E N U");
         for(i=0; i<=20; i++) System.out.printf(znak);
+        */
+        System.out.println("###################### \n" +
+                           "#                    # \n" +
+                           "# B I N F -- M E N U # \n" +
+                           "#                    # \n" +
+                           "######################");
 
         System.out.println("\n 1 - status \n 2 - app bbc -> drc \n 3 - db bbc -> drc \n 4 - app drc -> bbc \n 5 - db drc -> bbc");
 
@@ -24,12 +31,12 @@ public class Main {
         return choice.next();
     }
 
-    public static boolean  make(int choice) {
+    public static void  make(int choice) {
         try {
             System.out.println("tva volba: " + choice);
         }
         catch (NumberFormatException ex) {
-            return false;
+            System.out.println("zvol cislo moulo");
         }
     }
 }
