f493c0c6f8b191b9ff2e174c73e5da6ca9a83bfb
dposlt <david.poslt@gmail.com>
2020-10-25 14:26:44 +0100

    change scanner

--- binf/src/JavaAcademy/udemy/Main.java
+++ binf/src/JavaAcademy/udemy/Main.java
@@ -6,14 +6,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        if (isNumber(showMenu())==false) {
-            System.out.println("Zadej cislo 1 - 6 moulo");
-        } else {
-            System.out.println(showMenu());
-        }
+        System.out.println(showMenu());
     }
 
-    public static String showMenu() {
+    public static int showMenu() {
         /*
         String znak = "#";
         int i;
@@ -22,25 +18,16 @@ public class Main {
         for(i=0; i<=20; i++) System.out.printf(znak);
         */
         System.out.println("###################### \n" +
-                           "#                    # \n" +
-                           "# B I N F -- M E N U # \n" +
-                           "#                    # \n" +
-                           "######################");
+                "#                    # \n" +
+                "# B I N F -- M E N U # \n" +
+                "#                    # \n" +
+                "######################");
 
         System.out.println("\n 1 - status \n 2 - app bbc -> drc \n 3 - db bbc -> drc \n 4 - app drc -> bbc \n 5 - db drc -> bbc \n 6 - exit");
 
         System.out.printf("Zvol task: ");
-        Scanner choice = new Scanner(System.in);
-        return choice.next();
-    }
-
-    public static boolean isNumber(String choice) {
-        try {
-            int i = Integer.parseInt(choice);
-        }
-        catch (NumberFormatException ex) {
-            return false;
-        }
-        return true;
+        Scanner scan = new Scanner(System.in);
+        int menuNumber = scan.nextInt();
+        return menuNumber;
     }
 }
