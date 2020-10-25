f8511a7de7eb1a01007ef104c7e7964f576e41d7
dposlt <david.poslt@gmail.com>
2020-10-25 07:49:19 +0100

    fix isNumber

--- binf/src/JavaAcademy/udemy/Main.java
+++ binf/src/JavaAcademy/udemy/Main.java
@@ -6,13 +6,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        make(Integer.parseInt(showMenu()));
+        if (isNumber(showMenu())==false) {
+            System.out.println("Zadej cislo 1 - 6 moulo");
+        } else {
+            System.out.println(showMenu());
+        }
     }
 
     public static String showMenu() {
-        String znak = "#";
-
         /*
+        String znak = "#";
         int i;
         for(i=0; i<=20; i++) System.out.printf(znak);
         System.out.println("\n B I N F -- M E N U");
@@ -24,19 +27,20 @@ public class Main {
                            "#                    # \n" +
                            "######################");
 
-        System.out.println("\n 1 - status \n 2 - app bbc -> drc \n 3 - db bbc -> drc \n 4 - app drc -> bbc \n 5 - db drc -> bbc");
+        System.out.println("\n 1 - status \n 2 - app bbc -> drc \n 3 - db bbc -> drc \n 4 - app drc -> bbc \n 5 - db drc -> bbc \n 6 - exit");
 
         System.out.printf("Zvol task: ");
         Scanner choice = new Scanner(System.in);
         return choice.next();
     }
 
-    public static void  make(int choice) {
+    public static boolean isNumber(String choice) {
         try {
-            System.out.println("tva volba: " + choice);
+            int i = Integer.parseInt(choice);
         }
         catch (NumberFormatException ex) {
-            System.out.println("zvol cislo moulo");
+            return false;
         }
+        return true;
     }
 }
